cf946395221789a212e7ca0c91c1a09b2a770e52
ManpreetSingh18 <72294811+ManpreetSingh18@users.noreply.github.com>
2021-12-07 15:01:18 +0530

    Update conn.js

--- Mongo/src/db/conn.js
+++ Mongo/src/db/conn.js
@@ -4,7 +4,7 @@ mongoose.connect("mongodb://localhost:27017/regs",{useNewUrlParser:true,
 useUnifiedTopology:true
 
 }).then(()=>{
-    console.log(`Connection succesfull bc thapa`);
+    console.log(`Connection succesfull Eastablished`);
 }).catch((e)=>{
     console.log("Kat diya bhai:"+e);
 })
